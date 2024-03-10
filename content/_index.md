0ade1bd2784cc4895fe811b41ff1e0893b683790
Marsel Arduanov <m.arduanov@vk.team>
2024-03-10 14:23:25 +0500

    home template

--- content/_index.md
+++ content/_index.md
@@ -4,14 +4,4 @@ layout: hextra-home
 ---
 
 
-<div class="hx-mt-6 hx-mb-6">
-  Build modern websites&nbsp;<br class="sm:hx-block hx-hidden" />with Markdown and Hugo
-</div>
-
-
-
-<div class="hx-mb-12">
-  Fast, batteries-included Hugo theme&nbsp;<br class="sm:hx-block hx-hidden" />for creating beautiful static websites
-</div>
-
-
+<div class="hx-text-6xl hx-pb-24">Welcome!</div>
--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,3 @@
-// const themeDir = __dirname;
-//
-// module.exports = {
-//     plugins: [
-//         require('postcss-import'),
-//         require('@tailwindcss/nesting'),
-//         require('tailwindcss')(__dirname + '/tailwind.config.js'),
-//         require('autoprefixer')({
-//             path: [themeDir]
-//         })
-//     ]
-// }
-
-
 let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
 const tailwind = require('tailwindcss')(tailwindConfig);
 const autoprefixer = require('autoprefixer');
